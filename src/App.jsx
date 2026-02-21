ef81d8f8f64fbcdd2aa46239b0e4db119e441f05
Tania Schulz <taniaschulzv@gmail.com>
2026-02-21 18:47:06 -0300

    create property

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './App.css'
+import AppRouter from './routes/AppRouter';
 
 function App() {
   
-
   return (
     <>
       <AppRouter />
