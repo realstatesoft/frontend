cb17460f486434e6ff0e61562b82d36d11d5e14e
Tania Schulz <taniaschulzv@gmail.com>
2026-02-28 20:05:41 -0300

    papelera de propiedades

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -4,15 +4,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CreateProperty from "../pages/CreateProperty";
 import ShowProperty from "../pages/ShowProperty/ShowProperty";
 import HomePage from "../pages/LandingPage";
+import LandingPage from "../pages/LandingPage";
 import PropertiesPage from "../pages/PropertiesPage";
+import PropertiesTrashCan from "../pages/PropertiesTrashCan/PropertiesTrashCan";
 
 export default function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<LandingPage />} />
                 <Route path="/create-property" element={<CreateProperty />} />
                 <Route path="/show-property" element={<ShowProperty />} />
                 <Route path="/propiedades" element={<PropertiesPage />} />
+                <Route path="/trashcan" element={<PropertiesTrashCan />} />
             </Routes>
         </BrowserRouter>
     )
