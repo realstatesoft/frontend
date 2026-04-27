223c3fd03ef1745b64ee760d0c0f9896ab8c29cf
OttoEnzler <enzler.otto@gmail.com>
2026-04-27 17:56:13 -0300

    fix: unwrap API responses in searchPreferencesApi

--- src/services/search/searchPreferencesApi.js
+++ src/services/search/searchPreferencesApi.js
@@ -3,13 +3,13 @@ import api from "../api";
 const BASE = "/search-preferences";
 
 export const searchPreferencesApi = {
-  create: (data) => api.post(BASE, data),
+  create: (data) => api.post(BASE, data).then(res => res.data),
 
-  getMine: (params) => api.get(`${BASE}/me`, { params }),
+  getMine: (params) => api.get(`${BASE}/me`, { params }).then(res => res.data),
 
-  update: (id, data) => api.put(`${BASE}/${id}`, data),
+  update: (id, data) => api.put(`${BASE}/${id}`, data).then(res => res.data),
 
-  delete: (id) => api.delete(`${BASE}/${id}`),
+  delete: (id) => api.delete(`${BASE}/${id}`).then(res => res.data),
 };
 
 export default searchPreferencesApi;
