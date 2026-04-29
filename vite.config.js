e79b55102f8f23bd7d1bd382b57ece470d3e4c20
Nicolas_Ortiz <149315983+Nicolas-Ortiz-12@users.noreply.github.com>
2026-04-29 19:27:36 -0300

    Update Vite config for development and preview settings

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+
   server: {
+    host: true,
+    allowedHosts: mode === 'development' ? true : ['openroof.duckdns.org'],
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
@@ -13,6 +15,12 @@ export default defineConfig({
       }
     }
   },
+
+  preview: {
+    host: true,
+    allowedHosts: ['openroof.duckdns.org'],
+  },
+
   css: {
     preprocessorOptions: {
       scss: {
@@ -20,6 +28,7 @@ export default defineConfig({
       },
     },
   },
+
   test: {
     environment: 'happy-dom',
     globals: true,
@@ -43,4 +52,4 @@ export default defineConfig({
       },
     },
   },
-})
+}))
